a014c672bcadcb225890f9b78a15de8306861c6a
Xiaoying Pu <xp002@bucknell.edu>
2016-05-20 15:16:50 -0600

    no luck

--- surf_flann_based_matcher.cpp
+++ surf_flann_based_matcher.cpp
@@ -8,11 +8,12 @@
 #include <stdio.h>
 #include <iostream>
 // these are under /usr/local/include
-#include <opencv2/opencv_modules.hpp>
+//#include <opencv2/opencv_modules.hpp>
 #include <opencv2/xfeatures2d.hpp>      // since SURF's moved
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/xfeatures2d/nonfree.hpp>
+#include <opencv2/opencv.hpp>
+//#include <opencv2/core/core.hpp>
+//#include <opencv2/highgui/highgui.hpp>
+//#include <opencv2/xfeatures2d/nonfree.hpp>
 
 //#ifndef HAVE_OPENCV_NONFREE
 //
@@ -68,48 +69,48 @@ int main( int argc, char** argv )
     surf->compute( img_2, keypoints_2, descriptors_2 );
 
     //-- Step 3: Matching descriptor vectors using FLANN matcher
-    FlannBasedMatcher matcher;
-    std::vector< DMatch > matches;
-    matcher.match( descriptors_1, descriptors_2, matches );
+    //FlannBasedMatcher matcher;
+    //std::vector< DMatch > matches;
+    //matcher.match( descriptors_1, descriptors_2, matches );
 
-    double max_dist = 0; double min_dist = 100;
+    //double max_dist = 0; double min_dist = 100;
 
-    //-- Quick calculation of max and min distances between keypoints
-    for( int i = 0; i < descriptors_1.rows; i++ )
-    { double dist = matches[i].distance;
-        if( dist < min_dist ) min_dist = dist;
-        if( dist > max_dist ) max_dist = dist;
-    }
+    ////-- Quick calculation of max and min distances between keypoints
+    //for( int i = 0; i < descriptors_1.rows; i++ )
+    //{ double dist = matches[i].distance;
+    //    if( dist < min_dist ) min_dist = dist;
+    //    if( dist > max_dist ) max_dist = dist;
+    //}
 
-    printf("-- Max dist : %f \n", max_dist );
-    printf("-- Min dist : %f \n", min_dist );
+    //printf("-- Max dist : %f \n", max_dist );
+    //printf("-- Min dist : %f \n", min_dist );
 
-    //-- Draw only "good" matches (i.e. whose distance is less than 2*min_dist,
-    //-- or a small arbitary value ( 0.02 ) in the event that min_dist is very
-    //-- small)
-    //-- PS.- radiusMatch can also be used here.
-    std::vector< DMatch > good_matches;
+    ////-- Draw only "good" matches (i.e. whose distance is less than 2*min_dist,
+    ////-- or a small arbitary value ( 0.02 ) in the event that min_dist is very
+    ////-- small)
+    ////-- PS.- radiusMatch can also be used here.
+    //std::vector< DMatch > good_matches;
 
-    for( int i = 0; i < descriptors_1.rows; i++ )
-    { if( matches[i].distance <= max(2*min_dist, 0.02) )
-        { good_matches.push_back( matches[i]); }
-    }
+    //for( int i = 0; i < descriptors_1.rows; i++ )
+    //{ if( matches[i].distance <= max(2*min_dist, 0.02) )
+    //    { good_matches.push_back( matches[i]); }
+    //}
 
-    //-- Draw only "good" matches
-    Mat img_matches;
-    drawMatches( img_1, keypoints_1, img_2, keypoints_2,
-            good_matches, img_matches, Scalar::all(-1), Scalar::all(-1),
-            std::vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
+    ////-- Draw only "good" matches
+    //Mat img_matches;
+    //drawMatches( img_1, keypoints_1, img_2, keypoints_2,
+    //        good_matches, img_matches, Scalar::all(-1), Scalar::all(-1),
+    //        std::vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
 
-    //-- Show detected matches
-    imshow( "Good Matches", img_matches );
+    ////-- Show detected matches
+    //imshow( "Good Matches", img_matches );
 
-    for( int i = 0; i < (int)good_matches.size(); i++ )
-    { printf( "-- Good Match [%d] Keypoint 1: %d  -- Keypoint 2: %d  \n", i, good_matches[i].queryIdx, good_matches[i].trainIdx ); }
+    //for( int i = 0; i < (int)good_matches.size(); i++ )
+    //{ printf( "-- Good Match [%d] Keypoint 1: %d  -- Keypoint 2: %d  \n", i, good_matches[i].queryIdx, good_matches[i].trainIdx ); }
 
-    waitKey(0);
+    //waitKey(0);
 
-    return 0;
+    //return 0;
 }
 
 /**
