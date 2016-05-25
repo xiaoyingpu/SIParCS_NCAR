a916538761f1f0b9a8277c67e338df0f9e1e303d
Xiaoying Pu <xp002@bucknell.edu>
2016-05-25 11:04:05 -0600

    fixed mse

--- dasgupta/rmsd_isomap.py
+++ dasgupta/rmsd_isomap.py
@@ -1,10 +1,12 @@
+import numpy as np
+import cv2
 import math, operator
 from PIL import Image        # import Image not supported
-
 import sys
 
 
-def rmsd(img_n1, img_n2):
+
+def rmsd_histogram(img_n1, img_n2):
     """
     img_n1: file name string
     img_n2
@@ -16,11 +18,28 @@ def rmsd(img_n1, img_n2):
 
     return rms
 
+
+def mse(filename1, filename2):
+    """
+    img1 and img2 have the same dimension
+    """
+    img1 = cv2.imread(filename1)
+    img2 = cv2.imread(filename2)
+
+    img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+
+    err = np.sum((img1.astype("float") - img2.astype("float")) ** 2)
+    err /= float(img1.shape[0] * img2.shape[1])
+    return err
+
+
+
 def main():
     if len(sys.argv) != 3:
         print("Usage: frameworkpython rmsd_isomap.py img1 img2")
         exit()
 
-    print(rmsd(sys.argv[1], sys.argv[2]))
+    print(mse(sys.argv[1], sys.argv[2]))
 
 main()
