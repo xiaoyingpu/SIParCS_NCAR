142c877f41b1c95a19fef32d8bd4b78fe1bbe986
Xiaoying Pu <xp002@bucknell.edu>
2016-05-26 15:52:54 -0600

    added extra argument

--- dasgupta/gdal_translate.py
+++ dasgupta/gdal_translate.py
@@ -2,12 +2,11 @@ import os, sys
 from subprocess import call
 
 f_list = []
-if not len(sys.argv) == 3:
-    print("Usage: frameworkpython gdal_translate.py <nc_dir> <tif_dir> ")
+if not len(sys.argv) == 4:
+    print("Usage: python gdal_translate.py <nc_dir> <tif_dir> <variable name>")
 
 
 for f in os.listdir(sys.argv[1]):
-	
     if f.endswith(".nc"):
         f_list.append(f)
 
@@ -16,7 +15,7 @@ os.chdir(sys.argv[1])
 for i in range(len(f_list)):
 
     c = ["gdal_translate"]
-    c.append("NETCDF:\"{}\":tas_trends_ann".format(f_list[i]))
+    c.append("NETCDF:\"{}\":{}".format(f_list[i], sys.argv[3]))     #variable name
     # use absolute path for the output tiff
     tif_path = os.path.abspath(sys.argv[2])
     if not os.path.exists(tif_path):
@@ -24,7 +23,7 @@ for i in range(len(f_list)):
     if not tif_path.endswith("/"):
         tif_path += "/"
 
-    tif_path += "{}.tif".format(f_list[i])  # add file name
+    tif_path += "{}-{}.tif".format(f_list[i], sys.argv[3])  # add file name
     c = c + ["-scale", "-ot", "byte", "-of", "gtiff", tif_path]
     print(c)
     call(c)
