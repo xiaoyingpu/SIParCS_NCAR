9f42904587b343d0a8cd6d9824edfe31261fc54c
Xiaoying Pu <xp002@bucknell.edu>
2016-05-27 13:05:17 -0600

    it plots something

--- dasgupta/manifold.py
+++ dasgupta/manifold.py
@@ -1,15 +1,18 @@
 from pylab import scatter,text,show,cm,figure
 from pylab import subplot,imshow,NullLocator
 from sklearn import manifold, datasets
-import os, sys, cv2
+import os, sys, cv2, re
 
 
 f_list = []
+label_list = []
 X = []
 
 for f in os.listdir(sys.argv[1]):    # img dir as commandline arg
-    f_list.append(f)
-
+    if f.endswith(".tif") or f.endswith(".png"):    # there could be more
+        f_list.append(f)
+        lbl = re.findall(r"#\d+", f)
+        label_list.append(lbl[0][1:])              # regex!
 # change working directory
 os.chdir(sys.argv[1])
 
@@ -17,7 +20,6 @@ for f in f_list:
     img = cv2.imread(f)
     X.append(list(img.flat))
 
-print("Length of dataset: {}".format(len(X)))
 
 # load the digits dataset
 # 901 samples, about 180 samples per class
@@ -28,13 +30,12 @@ print("Length of dataset: {}".format(len(X)))
 
 # running Isomap
 # 5 neighbours will be considered and reduction on a 2d space
-Y = manifold.Isomap(3, 2).fit_transform(X)
+Y = manifold.Isomap(5, 2).fit_transform(X)
 
 # plotting the result
 figure(1)
 scatter(Y[:,0], Y[:,1], c='k', alpha=0.3, s=10)
-#for i in range(Y.shape[0]):
-# text(Y[i, 0], Y[i, 1], str(color[i]),
-#      color=cm.Dark2(color[i] / 5.),
-#      fontdict={'weight': 'bold', 'size': 11})
+for i in range(Y.shape[0]):
+ text(Y[i, 0], Y[i, 1], str(label_list[i]),
+      fontdict={'weight': 'bold', 'size': 11})
 show()
