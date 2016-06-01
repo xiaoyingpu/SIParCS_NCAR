398009d60383b002b2cb97e7859e0b769b1c6477
Xiaoying Pu <xp002@bucknell.edu>
2016-06-01 11:27:03 -0600

    labels now don't overlap

--- dasgupta/manifold.py
+++ dasgupta/manifold.py
@@ -2,7 +2,8 @@
 #from pylab import subplot,imshow,NullLocator
 #http://stackoverflow.com/questions/25516325/non-overlapping-scatter-plot-labels-using-matplotlib
 import matplotlib
-matplotlib.use('TkAgg')
+matplotlib.use('tkagg')
+
 import matplotlib.pyplot as plt
 import numpy as np
 import math
@@ -23,19 +24,13 @@ for f in os.listdir(sys.argv[1]):    # img dir as commandline arg
         #lbl = re.findall(r"#\d+", f)
         lbl = f[6:10]
         # label_list.append(lbl[0][1:])              # regex!
-        label_list.append(f[1:7])
+        label_list.append(f[2:7])
 # change working directory
 os.chdir(sys.argv[1])
 
 for f in f_list:
     img = cv2.imread(f)
     X.append(list(img.flat))
-# load the digits dataset
-# 901 samples, about 180 samples per class
-# the digits represented 0,1,2,3,4
-#digits = datasets.load_digits(n_class=5)
-#X = digits.data
-#color = digits.target
 
 # running Isomap
 # 5 neighbours will be considered and reduction on a 2d space
@@ -45,31 +40,29 @@ Y = manifold.Isomap(5, 2).fit_transform(X)
 #figure(1)
 fig, ax = plt.subplots()
 ax.scatter(Y[:,0], Y[:,1])
-#ann = []
-#for i in range(len(label_list)):
-#    ann.append(ax.annotate(label_list[i], xy = (list(Y[:,0])[i], list(Y[:,1])[i])))
-
-#mask = np.zeros(fig.canvas.get_width_height(), bool)
-
-#fig.canvas.show()
-
-#for a in ann:
-#    bbox = a.get_window_extent()
-#    x0 = int(bbox.x0)
-#    x1 = int(math.ceil(bbox.x1))
-#    y0 = int(bbox.y0)
-#    y1 = int(math.ceil(bbox.y1))
-#
-#    s = np.s_[x0:x1+1, y0:y1+1]
-#    if np.any(mask[s]):
-#        a.set_visible(False)
-#    else:
-#        mask[s] = True
+ann = []
+for i in range(len(label_list)):
+    ann.append(ax.annotate(label_list[i], xy = (list(Y[:,0])[i], list(Y[:,1])[i])))
+mask = np.zeros(fig.canvas.get_width_height(), bool)
 
 plt.tight_layout()
+fig.canvas.draw()
+#plt.show()
+for a in ann:
+    bbox = a.get_window_extent()
+    x0 = int(bbox.x0)
+    x1 = int(math.ceil(bbox.x1))
+    y0 = int(bbox.y0)
+    y1 = int(math.ceil(bbox.y1))
 
-plt.scatter(Y[:,0], Y[:,1], c='k', alpha=0.3, s=10)
-for i in range(Y.shape[0]):
- plt.text(Y[i, 0], Y[i, 1], str(label_list[i]),
-      fontdict={'weight': 'bold', 'size': 11})
+    s = np.s_[x0:x1+1, y0:y1+1]
+    if np.any(mask[s]):
+        a.set_visible(False)
+    else:
+        mask[s] = True
 plt.show()
+
+#plt.scatter(Y[:,0], Y[:,1], c='k', alpha=0.3, s=10)
+#for i in range(Y.shape[0]):
+# plt.text(Y[i, 0], Y[i, 1], str(label_list[i]),
+#      fontdict={'weight': 'bold', 'size': 11})
