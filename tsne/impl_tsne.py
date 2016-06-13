d55bf1dca1a948b6e4184685fef45e0eb11c666f
Xiaoying Pu <xp002@bucknell.edu>
2016-06-13 14:49:06 -0600

    attempt to label the t-SNE

--- tsne/impl_tsne.py
+++ tsne/impl_tsne.py
@@ -31,7 +31,8 @@ sns.set_context("notebook", font_scale=1.5,
                 rc={"lines.linewidth": 2.5})
 
 import os, sys, cv2
-
+import itertools
+from skimage.measure import compare_ssim  as ssim
 
 
 def get_model(s):
@@ -47,23 +48,26 @@ def color_index(f_list):
             d[m] = i
             i += 1
         y.append(d[m])
-    print y
     return y
 
 
-def scatter(x, colors):
+def scatter(x):
     # We choose a color palette with seaborn.
     palette = np.array(sns.color_palette("hls", 25))
 
     # We create a scatter plot.
     f = plt.figure(figsize=(8, 8))
     ax = plt.subplot(aspect='equal')
-    sc = ax.scatter(x[:,0], x[:,1], lw=0, s=40,
-                    c=palette[colors.astype(np.int)])
+    for i in range(len(x)):
+        ax.scatter(x[i][:,0], x[0][:,1], lw=0, s=40,
+                    c = palette[i],
+                    label = x[i][:,2][0])
     plt.xlim(-25, 25)
     plt.ylim(-25, 25)
+    plt.legend()
     ax.axis('off')
     ax.axis('tight')
+
 
     # We add the labels for each digit.
     txts = []
@@ -96,10 +100,27 @@ os.chdir(sys.argv[1])
 
 
 N = len(f_list)
+X = np.zeros((N,N))
 
-for f in f_list:
-    img = cv2.imread(f)
-    X.append(list(img.flat))
+for i_tuple in itertools.combinations(range(len(f_list)), 2):
+    i, j = i_tuple
+    img1 = cv2.imread(f_list[i])
+    img2 = cv2.imread(f_list[j])
+    # to grey scale
+    img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+
+    s = 1 - ssim(img1, img2)
+
+    # symmetric matrix!
+    # not sparse anymore!!!!
+    X[i][j] = s
+    X[j][i] = s
+#for f in f_list:
+#    img = cv2.imread(f)
+#    X.append(list(img.flat))
+
+# color dictionary: model name -> color code
 y = np.hstack(color_index(f_list))
 # We first reorder the data points according to the handwritten numbers.
 #X = np.vstack([digits.data[digits.target==i]
@@ -107,8 +128,24 @@ y = np.hstack(color_index(f_list))
 #y = np.hstack([digits.target[digits.target==i]
 #                   for i in range(10)])
 
-digits_proj = TSNE(random_state=RS).fit_transform(X)
+digits_proj = TSNE(random_state=RS, metric="precomputed").fit_transform(X)
 
 
-scatter(digits_proj, y)
+# build data frame:
+df = {}
+for i in range(len(y)):
+    df[i] = []
+
+for i in range(N):
+    m = get_model(f_list(i))
+    index = y[m]
+    x = digits_proj[:,0]
+    y = digits_proj[:,1]
+    df[index] = append([x,y,m])
+
+for i in range(len(y)):
+    df[i] = np.array(df[i])
+
+scatter(df, y)
 plt.show()
+
