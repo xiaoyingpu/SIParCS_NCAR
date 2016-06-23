a14183f1115a621f7ea5e1c6af67376c2f43fc83
Xiaoying Pu <xp002@bucknell.edu>
2016-06-23 13:03:16 -0600

    beginning to see how the quad expr works

--- gurobi/miqp.py
+++ gurobi/miqp.py
@@ -16,11 +16,22 @@ for i in range(n_item):
 for tupl in itertools.combinations(range(0, 4),2):
     print tupl
     i, j = tupl
-    r.append(m.addVar(vType = GRB.BINARY, obj = 1.0, name = "r_{}{}".format(i, j) ))
+    r.append(m.addVar(vType = GRB.BINARY, obj = 1.0, name = "r_{},{}".format(i, j) ))
 
 z = x + y + r
 
 m.update()
 
 
+# set objective
+# quad expression and minimize cost function
+quad_expr = None
 
+m.setObjective(quad_expr,GRB.MINIMIZE)
+
+
+
+
+
+# add constraints
+m.optimize()
