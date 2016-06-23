f5ec6ff294670772e7217124c795cd4d8b74c51b
Xiaoying Pu <xp002@bucknell.edu>
2016-06-23 12:14:32 -0600

    who knows if miqp would work

---
+++ gurobi/miqp.py
@@ -0,0 +1,26 @@
+from gurobipy import *
+import itertools
+
+m = Model("MIQP")
+
+n_item = 3
+x = []
+y = []
+r = []
+NEGATIVE = -10
+for i in range(n_item):
+    x.append(m.addVar(lb=NEGATIVE, ub = GRB.INFINITY, obj = 1.0, name="x_{}".format(i)))
+    y.append(m.addVar(lb=NEGATIVE, ub = GRB.INFINITY, obj = 1.0, name="y_{}".format(i)))
+
+
+for tupl in itertools.combinations(range(0, 4),2):
+    print tupl
+    i, j = tupl
+    r.append(m.addVar(vType = GRB.BINARY, obj = 1.0, name = "r_{}{}".format(i, j) ))
+
+z = x + y + r
+
+m.update()
+
+
+
