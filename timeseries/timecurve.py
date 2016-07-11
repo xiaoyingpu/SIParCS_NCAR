be7eb87751c9838360b99bba3269b3ea75fc0342
Xiaoying Pu <xp002@bucknell.edu>
2016-07-11 10:31:00 -0600

    dat netcdf doesn't even hhave unit for time dimension

--- timeseries/timecurve.py
+++ timeseries/timecurve.py
@@ -1,4 +1,5 @@
-from netCDF4 import Dataset
+from netCDF4 import Dataset, netcdftime
+from skimage.measure import compare_ssim as ssim
 import os
 
 var = "sic"
@@ -15,13 +16,29 @@ fh = Dataset(Walsh, mode="r")
 aice = fh.variables[var][:] # now numpy array
 aice = aice.filled(fill_value = 0)
 
+
+# get dat time
+nctime = fh.variables["time"][:]
+t_unit = fh.variavles["time"].units
+try:
+    calendar = fh.variables["time"].calendar
+except AttributeError:
+    calendar = u"gregorian"
+
+date_var = []
+date_var.append(netcdftime.num2date(nctime, units = t_unit, calendar = calendar))
+
+
 N = len(aice[:,0,0])        # 1700+ timestamps
 
 # need timestamps instead of indices
+# the movie analogy
+prev_frame = aice[0,:,:]
 
-
-for i in range(N):
-
+for i in range(1, N):
+    cur_frame = aice[i,:,:]
+    dist = ssim(cur_frame, prev_frame)
+    prev_frame = cur_frame
 
 
 
