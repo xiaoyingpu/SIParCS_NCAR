d6946b9d5d6aac0d681a6a151abbcb84440a41de
Xiaoying Pu <xp002@bucknell.edu>
2016-07-11 11:18:53 -0600

    similarity matrix next

--- timeseries/timecurve.py
+++ timeseries/timecurve.py
@@ -3,6 +3,19 @@ from netCDF4 import Dataset, num2date
 from skimage.measure import compare_ssim as ssim
 import os
 
+
+def get_datetime(d):
+    """
+    [YYYYMM]
+    """
+    d = str(int(d))
+    yyyy = int(d[:4])
+    mm = int(d[4:])
+    return datetime.date(year= yyyy, month = mm, day = 1)
+
+
+
+
 var = "sic"
 path = "/Users/puxiaoadmin/cmip5/timeseries"
 Walsh = "walsh_chapman.NH.seaice.187001-201112.nc"
@@ -20,16 +33,6 @@ aice = aice.filled(fill_value = 0)
 
 # ----------get date time-------------
 nctime = fh.variables["time"][:]
-
-t = []
-for d in nctime:
-    d = str(d)
-    yyyy = int(d[:4])
-    mm = int(d[4:])
-    date = datetime.date(year= yyyy, month = mm, day = 1)
-    t.append(date)
-
-print t[:4]
 #try:
 #    t_unit = fh.variavles["time"].units
 #    calendar = fh.variables["time"].calendar
@@ -41,23 +44,15 @@ print t[:4]
 
 
 
-
-
-
-
 N = len(aice[:,0,0])        # 1700+ timestamps
 
-# need timestamps instead of indices
 # the movie analogy
-prev_frame = aice[0,:,:]
 
 for i in range(1, N):
+    date = get_datetime( nctime[i])
     cur_frame = aice[i,:,:]
     dist = ssim(cur_frame, prev_frame)
     prev_frame = cur_frame
-
-
-
 
 
 fh.close()
