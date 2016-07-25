53ac2770f148432b68bfdca9fa851f578be6aa29
Xiaoying Pu <xp002@bucknell.edu>
2016-07-25 09:36:48 -0600

    whatever those changes are

--- dasgupta/plot_projection.py
+++ dasgupta/plot_projection.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import math
 
-df = pd.read_csv("./csv/ssim_cmip5_with_label_psl.csv")
+df = pd.read_csv("./csv/ssim_cmip5_190_metric_mds.csv")
 print df
 grp = df.groupby("label")
 
@@ -14,29 +14,29 @@ fig, ax = plt.subplots(figsize=(20,10))
 #palette = np.array(sns.color_palette("hls", 25))
 for name, group in grp:
     plt.plot(group.x, group.y, marker = ".",ms = 10, linestyle = " ", label = name)
-
+# was 10 for ms
 #ax.legend()
 
-ann = []
-N = len(df)
-for i in range(N):
-    ann.append(ax.annotate(df.label[i], xy = (df.x[i],df.y[i])))
-mask = np.zeros(fig.canvas.get_width_height(), bool)
-
-plt.tight_layout()
-#plt.legend()
-# overlapping labels removal
-fig.canvas.draw()
-for a in ann:
-    bbox = a.get_window_extent()
-    x0 = int(bbox.x0)
-    x1 = int(math.ceil(bbox.x1))
-    y0 = int(bbox.y0)
-    y1 = int(math.ceil(bbox.y1))
-
-    s = np.s_[x0:x1+1, y0:y1+1]
-    if np.any(mask[s]):
-        a.set_visible(False)
-    else:
-        mask[s] = True
+#ann = []
+#N = len(df)
+#for i in range(N):
+#    ann.append(ax.annotate(df.label[i], xy = (df.x[i],df.y[i])))
+#mask = np.zeros(fig.canvas.get_width_height(), bool)
+#
+#plt.tight_layout()
+##plt.legend()
+## overlapping labels removal
+#fig.canvas.draw()
+#for a in ann:
+#    bbox = a.get_window_extent()
+#    x0 = int(bbox.x0)
+#    x1 = int(math.ceil(bbox.x1))
+#    y0 = int(bbox.y0)
+#    y1 = int(math.ceil(bbox.y1))
+#
+#    s = np.s_[x0:x1+1, y0:y1+1]
+#    if np.any(mask[s]):
+#        a.set_visible(False)
+#    else:
+#        mask[s] = True
 plt.show()
